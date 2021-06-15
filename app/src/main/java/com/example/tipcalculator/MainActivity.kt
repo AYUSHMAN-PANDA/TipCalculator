cbda8424eee55b036009f72129aae1c1e3427391
Aayush <panda.ayushman@gmail.com>
2021-06-16 00:50:18 +0530

    app fully works

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.tipcalculator
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.tipcalculator.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener {
+            calculateTip()
+        }
+    }
+
+    private fun calculateTip() {
+        val text = binding.costOfService.text.toString()
+        val cost = text.toDoubleOrNull()
+        if (cost == null){
+            binding.amount.text=""
+            return
+        }
+
+        val tip = binding.RadioGrp.checkedRadioButtonId
+        val tipPer = when(tip){
+            R.id.option_20 -> 0.20
+            R.id.option_15 -> 0.15
+            else -> 0.10
+        }
+
+        var totalTip= tipPer * cost
+
+        val roundUp = binding.roundOf.isChecked
+        if(roundUp){
+            totalTip = kotlin.math.ceil(totalTip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(totalTip)
+        binding.amount.text = getString(R.string.tip_amount, formattedTip)
+
+
     }
 }
